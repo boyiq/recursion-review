0670c9dfff0c180eca93d5f70aa6d2d1d7d60369
Taylor <taylor.sheets3@gmail.com>
2022-09-06 12:32:34 -0700

    Pre-lunchtime changes - troubleshooting object cases

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,7 +2,44 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
-
+/*
+data types: undefined, null, numbers, strings, booleans, arrays, objects
+*/
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (typeof obj === 'boolean' ||
+  (typeof obj === 'number' && obj !== NaN)) {
+    return obj.toString();
+  }
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+  if (typeof obj === 'undefined' || typeof obj === 'function') {
+    return undefined;
+  }
+  if (obj === null || obj === NaN) {
+    return 'null';
+  }
+  if (typeof obj === 'object' && obj !== null) {
+    if (Array.isArray(obj)) {
+      var stringContent = '';
+      _.each(obj, function(element) {
+        if (typeof element === 'undefined' || typeof element === 'function') {
+          stringContent = stringContent + 'null' + ',';
+        } else {
+          stringContent = stringContent + stringifyJSON(element) + ',';
+        }
+      });
+      stringContent = stringContent.slice(0, stringContent.length - 1);
+      return '[' + stringContent + ']';
+    } else {
+      var stringContent = '';
+      _.each(obj, function(key) {
+        if (typeof obj[key] !== 'undefined' && typeof obj[key] !== 'function') {
+          stringContent = stringContent + stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ',';
+        }
+      });
+      stringContent = stringContent.slice(0, stringContent.length - 1);
+      return '{' + stringContent + '}';
+    }
+  }
 };
